1a67d61f274a5ac24299c6576654eb2549c1d2a4
Kevin Wang <chunkai1312@gmail.com>
2016-08-27 16:50:26 +0800

    refactor: use standard.js style

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ const router = require('koa-router')()
 const thing = require('../app/controllers/thing')
 
 module.exports = function (app) {
-  router.get('/', function * () { yield this.render('index', { title: 'Koa' }); })
+  router.get('/', function * () { yield this.render('index', { title: 'Koa' }) })
   router.get('/api/things', thing.index)
   router.post('/api/things', thing.create)
   router.get('/api/things/:id', thing.show)
--- test/thing.test.js
+++ test/thing.test.js
@@ -1,11 +1,11 @@
-'use strict';  /* eslint import/no-extraneous-dependencies: 0 */
+'use strict'
 
-const expect = require('chai').expect;
-const request = require('supertest-as-promised');
-const app = require('../app');
+const expect = require('chai').expect
+const request = require('supertest-as-promised')
+const app = require('../app')
 
 describe('Thing API:', () => {
-  let thing;
+  let thing
 
   describe('GET /api/things', () => {
     it('should respond with JSON array', () => {
@@ -13,9 +13,9 @@ describe('Thing API:', () => {
         .get('/api/things')
         .expect('Content-Type', /json/)
         .expect(200)
-        .then(res => expect(res.body).to.be.instanceOf(Array));
-    });
-  });
+        .then(res => expect(res.body).to.be.instanceOf(Array))
+    })
+  })
 
   describe('POST /api/things', () => {
     it('should respond with the newly created thing', () => {
@@ -23,18 +23,18 @@ describe('Thing API:', () => {
         .post('/api/things')
         .send({
           name: 'New Thing',
-          info: 'This is the brand new thing!!!',
+          info: 'This is the brand new thing!!!'
         })
         .expect('Content-Type', /json/)
         .expect(201)
         .then(res => {
-          thing = res.body;
-          expect(thing).to.have.property('id');
-          expect(thing).to.have.property('name').that.equal('New Thing');
-          expect(thing).to.have.property('info').that.equal('This is the brand new thing!!!');
-        });
-    });
-  });
+          thing = res.body
+          expect(thing).to.have.property('id')
+          expect(thing).to.have.property('name').that.equal('New Thing')
+          expect(thing).to.have.property('info').that.equal('This is the brand new thing!!!')
+        })
+    })
+  })
 
   describe('GET /api/things/:id', () => {
     it('should respond with the requested thing', () => {
@@ -43,12 +43,12 @@ describe('Thing API:', () => {
         .expect('Content-Type', /json/)
         .expect(200)
         .then(res => {
-          expect(res.body.id).to.equal(thing.id);
-          expect(res.body.name).to.equal(thing.name);
-          expect(res.body.info).to.equal(thing.info);
-        });
-    });
-  });
+          expect(res.body.id).to.equal(thing.id)
+          expect(res.body.name).to.equal(thing.name)
+          expect(res.body.info).to.equal(thing.info)
+        })
+    })
+  })
 
   describe('PUT /api/things/:id', () => {
     it('should respond with the updated thing', () => {
@@ -56,29 +56,29 @@ describe('Thing API:', () => {
         .put(`/api/things/${thing.id}`)
         .send({
           name: 'Updated Thing',
-          info: 'This is the updated thing!!!',
+          info: 'This is the updated thing!!!'
         })
         .expect('Content-Type', /json/)
         .expect(200)
         .then(res => {
-          expect(res.body.id).to.equal(thing.id);
-          expect(res.body.name).to.equal('Updated Thing');
-          expect(res.body.info).to.equal('This is the updated thing!!!');
-        });
-    });
-  });
+          expect(res.body.id).to.equal(thing.id)
+          expect(res.body.name).to.equal('Updated Thing')
+          expect(res.body.info).to.equal('This is the updated thing!!!')
+        })
+    })
+  })
 
   describe('DELETE /api/things/:id', () => {
     it('should respond with 204 on successful removal', () => {
       return request(app.listen())
         .delete(`/api/things/${thing.id}`)
-        .expect(204);
-    });
+        .expect(204)
+    })
 
     it('should respond with 404 when thing does not exist', () => {
       return request(app.listen())
         .delete(`/api/things/${thing.id}`)
-        .expect(404);
-    });
-  });
-});
+        .expect(404)
+    })
+  })
+})
